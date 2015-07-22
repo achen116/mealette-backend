d30928db02f121ad161ed43ea8850ff333a38549
Arielle Chen & Kelvin Gann <pair+achen116.klvngnn@devbootcamp.com>
2015-07-22 14:18:04 -0700

    clean up p statements

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -30,32 +30,16 @@ class ApiController < ApplicationController
     y = 9
     result_range = x..y
 
-    p '^'*100
-    p result_range
-    p '^'*100
-
     counter = params[:repopulate].to_i if params[:repopulate]
 
     if params[:repopulate]
         x += counter * 10
         y += counter * 10
         result_range = x..y
-        p '$'*100
-        p result_range
-        p '$'*100
-
     end
-
-        p '*'*100
-        p result_range
-        p '*'*100
 
     # Yelp API Query ==========================================================
     result = Yelp.client.search_by_coordinates(coordinates, category)
-
-    p "-- "
-    p result.businesses.length
-    p "-- "
 
     if !result.businesses || result.businesses.length < 10
       return render json: {error: "No businesses found", status: 400}
